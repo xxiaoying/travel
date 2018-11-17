cee6fc8d0c386c59e5843d532336d2cc140a441e
xxiaoying <you@example.com>
2018-11-17 11:41:55 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 300毫秒点击延迟问题
+import fastClick from 'fastclick'
+// 图片轮播
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// 重置移动端不同手机默认样式
+import 'styles/reset.css'
+// 不同dpr手机 1px问题
+import 'styles/border.css'
+// 图标样式
+import 'styles/iconfont.css'
+// 图片轮播样式
+import 'swiper/dist/css/swiper.css'
+
+Vue.config.productionTip = false
+// 使用快速点击
+fastClick.attach(document.body)
+// 使用图片轮播
+Vue.use(VueAwesomeSwiper)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,363 @@
+<template>
+  <div>
+    <header class="header">
+      <div class="header-left">
+        <span class="iconfont icon-back">&#xe624;</span>
+      </div>
+      <div class="header-input">
+        <span class="iconfont icon-search">&#xe632;</span>
+        输入城市/景点/游玩主题
+      </div>
+      <router-link to="/city">
+        <div class="header-right">
+          <!-- {{this.$store.state.currentCity}} -->
+          南京
+          <span class="iconfont icon-select">&#xe64a;</span>
+        </div>
+      </router-link>
+    </header>
+    <div>
+      <!-- 图片轮播 -->
+      <section class="swiper">
+        <swiper :options="swiperOption" v-if="showSwiper">
+          <swiper-slide v-for="swiper in swiperList" :key="swiper.id">
+            <img class="swiper-img" :src="swiper.imgUrl">
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+      </section>
+      <!-- 图片轮播 -->
+      <section class="icons">
+        <swiper :options="swiperOptionIcons">
+          <swiper-slide v-for="(page, index) in pages" :key="index">
+            <div class="icon" v-for="icon in page" :key="icon.id">
+              <div class="icon-img">
+                <img class="icon-img-content" :src="icon.imgUrl">
+              </div>
+              <p class="icon-p">{{icon.text}}</p>
+            </div>
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+      </section>
+      <!-- 定位 + 必游榜单 -->
+      <section class="gps-hot border-top">
+        <div class="gps border-right">
+          <span class="iconfont">&#xe678;</span>定位失败
+        </div>
+        <div class="hot">
+          <span class="iconfont">&#xe678;</span>必游榜单
+        </div>
+      </section>
+      <!-- 限时抢购 + 打卡圣地 -->
+      <section class="activity border-topbottom">
+        <div class="img-warpper  border-right">
+          <div  class="img-content">
+            <img src="http://img1.qunarzz.com/piao/fusion/1811/12/999862443f79a902.png">
+          </div>
+        </div>
+        <div class="img-warpper">
+          <div  class="img-content">
+            <img src="http://img1.qunarzz.com/piao/fusion/1811/d5/20dc542eb839c702.png">
+          </div>
+        </div>
+      </section>
+      <!-- 本周热门 -->
+      <section class="scenic-spot-list">
+        <div class="title">
+          <h3><span class="iconfont">&#xe678;</span>本周热门榜单</h3>
+          <h5>全部榜单<span>&#xe678;</span></h5>
+        </div>
+        <ul class="hot-sale-list">
+          <li class="hot-sale-item" v-for="item in hotList" :key="item.id">
+            <div v-if="item.top">
+              <img class="top-img" :src="item.top">
+            </div>
+            <div class="img-content">
+              <img :src="item.imgUrl">
+            </div>
+            <h4 class="name">{{item.title}}</h4>
+            <div class="ticket"><span class="unit">￥<em class="price">{{item.price}}</em></span>起</div>
+          </li>
+        </ul>
+      </section>
+      <!-- 猜你喜欢 -->
+      <section class="scenic-spot-list">
+        <div class="title">
+          <h3><span class="iconfont">&#xe678;</span>猜你喜欢</h3>
+        </div>
+        <ul class="like-list">
+          <li class="like-item border-bottom" v-for="item in hotList" :key="item.id">
+            <!-- <div v-if="item.top">
+              <img class="top-img" :src="item.top">
+            </div> -->
+            <div class="img-warpper">
+              <img class="img-content" :src="item.imgUrl">
+            </div>
+            <div class="info">
+              <h3 class="name">{{item.title}}</h3>
+              <div class="level">
+                <span class="level-scope">
+                  <strong class="score">
+                    <span class="iconfont">&#xe64a;&#xe64a;&#xe64a;&#xe64a;&#xe64a;</span>
+                  </strong>
+                  <span>
+                    <span class="iconfont">&#xe64a;&#xe64a;&#xe64a;&#xe64a;&#xe64a;</span>
+                  </span>
+                </span>
+                <span>111条评论</span>
+              </div>
+              <div class="price-addr">
+                <div class="ticket">
+                  <span class="unit">￥<em class="price">{{item.price}}</em></span>起
+                </div>
+                <div>浦口区</div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </section>
+      <!-- 周末去哪儿 -->
+      <section style="height: 200px;"></section>
+    </div>
+  </div>
+</template>
+
+<script>
+// 首页
+import axios from 'axios'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 1500
+      },
+      swiperOptionIcons: {
+        pagination: '.swiper-pagination'
+      },
+      swiperList: [],
+      iconList: [],
+      hotList: [],
+      recommendList: [],
+      weekendList: []
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.swiperList.length
+    },
+    pages () {
+      let pages = []
+      this.iconList.forEach((item, i) => {
+        let index = Math.floor(i / 8)
+        if (!pages[index]) {
+          pages[index] = []
+        }
+        pages[index].push(item)
+      })
+      return pages
+    }
+  },
+  methods: {
+    getIndexDatas () {
+      // axios.get('/api/index.json?city=' + this.currentCity).then(this.getIndexInfoSuccess)
+      axios.get('/api/index.json').then(this.getIndexInfoSuccess)
+    },
+    getIndexInfoSuccess (res) {
+      const resData = res.data.data
+      this.swiperList = resData.swiperList
+      this.iconList = resData.iconList
+      this.hotList = resData.hotList
+      this.recommendList = resData.recommendList
+      this.weekendList = resData.weekendList
+    }
+  },
+  mounted () {
+    this.getIndexDatas()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+  >>> .swiper-pagination-bullet
+    width: .12rem;
+    height: .12rem;
+  .header
+    display: flex
+    background: $bgHeaderColor
+    color: #fff
+    height: .86rem
+    line-height: .86rem
+    .header-input
+      flex: 1
+      border-radius: .04rem
+      background: #fff
+      height: .62rem
+      line-height: .62rem
+      margin-top: .12rem
+      padding-left: .15rem
+      color: #dcdcdc
+    .header-left
+      width: .64rem
+      padding-left: .2rem
+      .icon-back
+        font-size: .32rem
+        font-weight: bold
+    .header-right
+      min-width: 1.28rem
+      text-align: center
+      text-align: center
+      padding: 0 .1rem
+      color: #fff
+      .icon-select
+        font-size: .1rem
+        display: inline-block
+        margin-left: -.04rem
+  .swiper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .swiper
+    height: 0
+    padding-bottom: 26.67%
+    background: #f3f3f3
+    .swiper-img
+      width: 100%
+  .icons >>> .swiper-container
+    position: static
+  .icons >>> .swiper-pagination-bullets
+    bottom: .04rem
+  .icons
+    width: 100%
+    // 预留5%显示轮播切换
+    padding-bottom: 55%
+    height: 0
+    position:relative
+    background: $bgColor
+    .icon
+      position: relative
+      float: left
+      width: 25%
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        left: 0
+        right: 0
+        top: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding-top: .3rem
+        .icon-img-content
+          display: block
+          height: 100%
+          margin: auto
+      .icon-p
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        text-align: center
+        line-height: .44rem
+  .gps-hot
+  .activity
+    display: flex
+    text-align: center
+    background: $bgColor
+    .gps
+    .hot
+      flex: 1
+      padding: .3rem
+  .activity
+    margin-top: .2rem
+    .img-warpper
+      flex: 1
+      img
+        width: 100%
+  .scenic-spot-list
+    margin-top: .2rem
+    background: $bgColor
+    padding-left: .24rem
+    .title
+      display: flex
+      line-height: .44rem
+      padding: .26rem .24rem .26rem 0
+      h3
+        flex: 1
+        font-size: .32rem
+      h5
+        color: $darkTextColor
+    .hot-sale-list
+      white-space: nowrap
+      overflow-x: scroll
+      li + li
+        margin-left: .12rem
+      li:last-child
+        margin-right: .24rem
+      // 子容器出现滚动条，如果使用的块元素需要设置为内联元素撑出宽度，如果使用float会失效
+      .hot-sale-item
+        display: inline-block
+        width: 2rem
+        position: relative
+        text-align: center
+        font-size: .24rem
+        .top-img
+          position: absolute
+          left: 0
+          top: 0
+          width: .84rem
+        .img-content
+          padding-bottom: 100%
+          height: 0
+          margin-top: .1rem
+          img
+            width: 100%
+        .name
+          line-height: .32rem
+          margin-top: .12rem
+        .ticket
+          color: $darkTextColor
+          padding: .04rem 0
+          margin-bottom: .2rem
+          .unit
+            color: $emColor
+          .price
+            font-size: .28rem
+    .like-list
+      .like-item
+        padding: .2rem 0
+        position: relative
+        display: flex
+        .img-warpper
+          width: 2rem
+          height: 2rem
+          .img-content
+            width: 100%
+        .info
+          flex: 1
+          padding: 0 .2rem
+          .name
+            font-size: .32rem
+            padding-top: .26rem
+          .level
+            padding: .2rem 0 .22rem
+            .level-scope
+              position: relative
+              width: 80px
+              height: 10px
+              display: inline-block
+              .score
+                color: $emColor
+                position: absolute
+                width: 50%
+                overflow: hidden
+          .price-addr
+            display: flex
+            .ticket
+              flex: 1
+              .unit
+                color: $emColor
+              .price
+                font-size: .38rem
+</style>
